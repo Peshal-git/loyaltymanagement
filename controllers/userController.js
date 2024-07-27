cc18b7c9ee79941d68f13ce2c3f50c1c78d7ae06
Peshal <ppeshalmani@gmail.com>
2024-07-27 09:32:39 +0545

    Handlebar date and time error handling

--- controllers/userController.js
+++ controllers/userController.js
@@ -18,19 +18,29 @@ hbs.registerHelper('startFromOne', function (value) {
 })
 
 hbs.registerHelper('makeDate', function (value) {
-    const formattedDate = value.toISOString().split('T')[0]
-    const parts = formattedDate.split('-')
-    const mmddyyyy = `${parts[1]}-${parts[2]}-${parts[0]}`
-    return mmddyyyy
+    if (value) {
+        const formattedDate = value.toISOString().split('T')[0]
+        const parts = formattedDate.split('-')
+        const mmddyyyy = `${parts[1]}-${parts[2]}-${parts[0]}`
+        return mmddyyyy
+    }
+    else {
+        return "Date not available"
+    }
 })
 
 hbs.registerHelper('makeTime', function (value) {
-    const date = new Date(value);
-    const hours = String(date.getHours()).padStart(2, '0');
-    const minutes = String(date.getMinutes()).padStart(2, '0');
-    const seconds = String(date.getSeconds()).padStart(2, '0');
-    const formattedTime = `${hours}:${minutes}:${seconds}`;
-    return formattedTime;
+    if (value) {
+        const date = new Date(value);
+        const hours = String(date.getHours()).padStart(2, '0');
+        const minutes = String(date.getMinutes()).padStart(2, '0');
+        const seconds = String(date.getSeconds()).padStart(2, '0');
+        const formattedTime = `${hours}:${minutes}:${seconds}`;
+        return formattedTime;
+    }
+    else {
+        return "Time not available"
+    }
 })
 
 const userRegister = async (req, res) => {
@@ -987,10 +997,10 @@ const checkPrivacyAndMarketing = async (req, res) => {
         } else {
             let privacyCreated = new Date()
             let marketingCreated = new Date()
-            if (userExits.privacy.createdAt) {
+            if (userExits?.privacy?.createdAt) {
                 privacyCreated = userExits.privacy.createdAt
             }
-            if (userExits.marketing.createdAt) {
+            if (userExits?.marketing?.createdAt) {
                 marketingCreated = userExits.marketing.createdAt
             }
             const updatedUserData = await User.findByIdAndUpdate({ _id: userExits._id }, {
