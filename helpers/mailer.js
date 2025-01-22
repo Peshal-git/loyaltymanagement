6c62424d19e6a8d6ebb2c82f8227b7e63ffb5099
Peshal <ppeshalmani@gmail.com>
2025-01-22 19:36:31 +0545

    Mailer support@dosink.com

--- helpers/mailer.js
+++ helpers/mailer.js
@@ -1,24 +1,12 @@
-// const nodemailer = require('nodemailer')
 const sendgridMailer = require('@sendgrid/mail')
 
 sendgridMailer.setApiKey(process.env.SENDGRID_API)
-
-// const transporter = nodemailer.createTransport({
-//     host: process.env.SMTP_HOST,
-//     port: process.env.SMTP_PORT,
-//     secure: false,
-//     requireTLS: true,
-//     auth: {
-//         user: process.env.SMTP_MAIL,
-//         pass: process.env.SMTP_PASSWORD
-//     }
-// })
 
 const sendMail = async (email, subject, content) => {
     try {
         var message = {
             from: {
-                name: 'Peshal',
+                name: 'support@dosink.com',
                 email: process.env.SENDGRID_MAIL
             },
             to: email,
@@ -26,17 +14,14 @@ const sendMail = async (email, subject, content) => {
             html: content
         }
 
-        // transporter.sendMail(message, (error, info) => {
-        //     if (error) {
-        //         console.log(error)
-        //     }
-        //     console.log('Mail Sent ', info.messageId)
-        // })
-
         await sendgridMailer.send(message)
-
+        console.log(`Email sent to ${email}`)
     } catch (error) {
-        console.log(error.message)
+        if (error.response) {
+            console.error('SendGrid Error:', error.response.body.errors || error.message);
+        } else {
+            console.error('Error:', error.message);
+        }
     }
 }
 
