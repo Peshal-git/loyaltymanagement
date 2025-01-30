67ddaa54e2aa4f79377ce7e4c37a6fbe4ff2507a
Peshal <ppeshalmani@gmail.com>
2025-01-30 08:27:49 +0545

    Mailer fixed

--- helpers/mailer.js
+++ helpers/mailer.js
@@ -6,7 +6,7 @@ const sendMail = async (email, subject, content) => {
     try {
         var message = {
             from: {
-                name: 'Peshal',
+                name: process.env.SENDGRID_NAME,
                 email: process.env.SENDGRID_MAIL
             },
             to: email,
