be67d21e5590c7987f5880a737fd48cbb488d908
Peshal <ppeshalmani@gmail.com>
2025-04-19 11:22:36 +0545

    Mobile uniqueness error fix

--- helpers/hbsHelpers.js
+++ helpers/hbsHelpers.js
@@ -1,185 +1,181 @@
-const hbs = require('hbs')
+const hbs = require("hbs");
 
-hbs.registerHelper('startFromOne', function (value) {
-    return parseInt(value) + 1
-})
-
-hbs.registerHelper('ifEquals', function(arg1, arg2, options) {
-    return arg1 === arg2 ? options.fn(this) : options.inverse(this);
+hbs.registerHelper("startFromOne", function (value) {
+  return parseInt(value) + 1;
 });
 
-hbs.registerHelper('makeDate', function (value) {
-    if (value) {
-        const date = new Date(value); 
-        if (!isNaN(date)) {
-            const formattedDate = date.toISOString().split('T')[0];
-            const parts = formattedDate.split('-');
-            const mmddyyyy = `${parts[0]}-${parts[1]}-${parts[2]}`;
-            return mmddyyyy;
-        }
-    }
-    return "N/A";
-})
+hbs.registerHelper("ifEquals", function (arg1, arg2, options) {
+  return arg1 === arg2 ? options.fn(this) : options.inverse(this);
+});
 
-hbs.registerHelper('makeTime', function (value) {
-    if (value) {
-        const date = new Date(value);
-        const hours = String(date.getHours()).padStart(2, '0');
-        const minutes = String(date.getMinutes()).padStart(2, '0');
-        const seconds = String(date.getSeconds()).padStart(2, '0');
-        const formattedTime = `${hours}:${minutes}:${seconds}`;
-        return formattedTime;
+hbs.registerHelper("makeDate", function (value) {
+  if (value) {
+    const date = new Date(value);
+    if (!isNaN(date)) {
+      const formattedDate = date.toISOString().split("T")[0];
+      const parts = formattedDate.split("-");
+      const mmddyyyy = `${parts[0]}-${parts[1]}-${parts[2]}`;
+      return mmddyyyy;
     }
-    else {
-        return "Time not available"
-    }
-})
+  }
+  return "N/A";
+});
 
-hbs.registerHelper('isActive', function (currentPage, page) {
-    return currentPage === page ? 'active' : '';
+hbs.registerHelper("makeTime", function (value) {
+  if (value) {
+    const date = new Date(value);
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+    const formattedTime = `${hours}:${minutes}:${seconds}`;
+    return formattedTime;
+  } else {
+    return "Time not available";
+  }
+});
+
+hbs.registerHelper("isActive", function (currentPage, page) {
+  return currentPage === page ? "active" : "";
 });
 
 hbs.registerHelper("formatPoints", function (points) {
-    if (points === undefined || isNaN(points)) {
-        return "0.00";
-    }
-    return points.toFixed(2);
+  if (points === undefined || isNaN(points)) {
+    return "0.00";
+  }
+  return points.toFixed(2);
 });
 
-hbs.registerHelper("getValue", function(transactionField, enteredField) {
-    return transactionField || enteredField;
+hbs.registerHelper("getValue", function (transactionField, enteredField) {
+  return transactionField || enteredField;
 });
 
-hbs.registerHelper("getTier", function(userTier, enteredField) {
-    return userTier || enteredField;
+hbs.registerHelper("getTier", function (userTier, enteredField) {
+  return userTier || enteredField;
 });
 
-hbs.registerHelper('lowercase', function(str) {
-    return str.toLowerCase();
+hbs.registerHelper("lowercase", function (str) {
+  return str.toLowerCase();
 });
 
 hbs.registerHelper("eq", function (a, b) {
-    return a === b;
+  return a === b;
 });
 
 hbs.registerHelper("makeType", function (value) {
-    if(value=="add"){
-        return 'Transaction'
-    }
-    if(value="subtract"){
-        return 'Redemption'
-    }
-    return 'Error'
-    
+  if (value == "add") {
+    return "Transaction";
+  }
+  if ((value = "subtract")) {
+    return "Redemption";
+  }
+  return "Error";
 });
 
-hbs.registerHelper('getColor', function (value) {
-    if(value=='add'){
-        return 'colorGreen'
-    }
-    if(value="subtract"){
-        return 'colorRed'
-    }
-    return 'Error'
-
+hbs.registerHelper("getColor", function (value) {
+  if (value == "add") {
+    return "colorGreen";
+  }
+  if ((value = "subtract")) {
+    return "colorRed";
+  }
+  return "Error";
 });
 
-hbs.registerHelper('getUsedOrGained', function (value) {
-    if(value=='add'){
-        return 'Gained: '
-    }
-    if(value="subtract"){
-        return 'Used: '
-    }
-    return 'Error'
-
+hbs.registerHelper("getUsedOrGained", function (value) {
+  if (value == "add") {
+    return "Gained: ";
+  }
+  if ((value = "subtract")) {
+    return "Used: ";
+  }
+  return "Error";
 });
 
-hbs.registerHelper('commaFormat', function (number) {
-    if (typeof number !== 'number') {
-        return number;
-    }
-    return Math.floor(number).toLocaleString('en-US');
+hbs.registerHelper("commaFormat", function (number) {
+  if (typeof number !== "number") {
+    return number;
+  }
+  return Math.floor(number).toLocaleString("en-US");
 });
 
-hbs.registerHelper('sanitizeId', function(name) {
-    return name.replace(/\s+/g, '-').toLowerCase();
+hbs.registerHelper("sanitizeId", function (name) {
+  return name.replace(/\s+/g, "-").toLowerCase();
 });
 
-hbs.registerHelper('concat', function (...args) {
-    return args.slice(0, -1).join('');
+hbs.registerHelper("concat", function (...args) {
+  return args.slice(0, -1).join("");
 });
 
-hbs.registerHelper('lessThan', function(a, b) {
-    if (a < b) {
-        return true;  
-    } else {
-        return false;
-    }
-})
-
-hbs.registerHelper('minus', function(a, b){
-    return Math.ceil(a-b);
-})
-
-hbs.registerHelper('isSelected', function (currentTier, tier) {
-    return currentTier === tier ? 'selected' : '';
+hbs.registerHelper("lessThan", function (a, b) {
+  if (a < b) {
+    return true;
+  } else {
+    return false;
+  }
 });
 
-hbs.registerHelper('divide', function (value1, value2) {
-    if (value2 === 0) return 0;
-    return value1 / value2;
-})
+hbs.registerHelper("minus", function (a, b) {
+  return Math.ceil(a - b);
+});
 
-hbs.registerHelper('progressOne', function (tier, basePoints) {
-    let pointsToBeShown
+hbs.registerHelper("isSelected", function (currentTier, tier) {
+  return currentTier === tier ? "selected" : "";
+});
 
-    switch (tier) {
-      case 'Balance':
-        pointsToBeShown = basePoints["Vitality"];
-        break;
-      case 'Vitality':
-        pointsToBeShown = basePoints["Harmony"];
-        break;
-      case 'Harmony':
-        pointsToBeShown = basePoints["Serenity"];
-        break;
-    }
-  
-    return pointsToBeShown
-})
+hbs.registerHelper("divide", function (value1, value2) {
+  if (value2 === 0) return 0;
+  return value1 / value2;
+});
 
-hbs.registerHelper('progressTwo', function (tier, basePoints) {
-    let pointsToBeShown
+hbs.registerHelper("progressOne", function (tier, basePoints) {
+  let pointsToBeShown;
 
-    switch (tier) {
-      case 'Balance':
-        pointsToBeShown = basePoints["Harmony"];
-        break;
-      case 'Vitality':
-        pointsToBeShown = basePoints["Serenity"];
-        break;
-    }
-    
-    return pointsToBeShown
-})
+  switch (tier) {
+    case "Balance":
+      pointsToBeShown = basePoints["Vitality"];
+      break;
+    case "Vitality":
+      pointsToBeShown = basePoints["Harmony"];
+      break;
+    case "Harmony":
+      pointsToBeShown = basePoints["Serenity"];
+      break;
+  }
 
-hbs.registerHelper('getTextColor', function (tier) {
-let textColor;
-    switch (tier) {
-        case 'Balance':
-            textColor = "balance-text";
-            break;
-        case 'Vitality':
-            textColor = "vitality-text";
-            break;
-        case 'Harmony':
-            textColor = "harmony-text";
-            break;
-        case 'Serenity':
-            textColor = "serenity-text";
-            break;
-    }
-    
-    return textColor
-})
+  return pointsToBeShown;
+});
+
+hbs.registerHelper("progressTwo", function (tier, basePoints) {
+  let pointsToBeShown;
+
+  switch (tier) {
+    case "Balance":
+      pointsToBeShown = basePoints["Harmony"];
+      break;
+    case "Vitality":
+      pointsToBeShown = basePoints["Serenity"];
+      break;
+  }
+
+  return pointsToBeShown;
+});
+
+hbs.registerHelper("getTextColor", function (tier) {
+  let textColor;
+  switch (tier) {
+    case "Balance":
+      textColor = "balance-text";
+      break;
+    case "Vitality":
+      textColor = "vitality-text";
+      break;
+    case "Harmony":
+      textColor = "harmony-text";
+      break;
+    case "Serenity":
+      textColor = "serenity-text";
+      break;
+  }
+
+  return textColor;
+});
--- models/userModel.js
+++ models/userModel.js
@@ -1,163 +1,190 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const SystemDataSchema = new mongoose.Schema({
+const SystemDataSchema = new mongoose.Schema(
+  {
     password: {
-        type: String,
+      type: String,
     },
     authentication: {
-        type: String
-    }
-}, {
-    timestamps: true
-})
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const PrivacySchema = new mongoose.Schema({
+const PrivacySchema = new mongoose.Schema(
+  {
     hasAcceptedPrivacyPolicy: {
-        type: Boolean
+      type: Boolean,
     },
     createdAt: {
-        type: Date,
-        required: true,
-        default: Date.now
-    }
-}, {
-    timestamps: true
-})
+      type: Date,
+      required: true,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const MarketingSchema = new mongoose.Schema({
+const MarketingSchema = new mongoose.Schema(
+  {
     hasGivenMarketingConsent: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false,
     },
     createdAt: {
-        type: Date,
-        required: true,
-        default: Date.now
-    }
-}, {
-    timestamps: true
-})
+      type: Date,
+      required: true,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const MemberInfoSchema = new mongoose.Schema({
-    pointsAvailable: {
-        type: Number,
-        default: 0
-    },
-    pointsForRedemptions: {
-        type: Number,
-        default: 0
-    },
-    lastVisit: {
-        type: Date
-    },
-    lastCommunication: {
-        type: Date
-    },
-    lastMarketingCommunication: {
-        type: Date
-    },
-    expiringPoints: {
-        type: Number
-    },
-    lastUsagePoints: {
-        type: Number
-    },
-    totalLifetimePoints: {
-        type: Number
-    },
-    status: {
-        type: String,
-        enum: ["Active", "Pending", "Inactive", "Upgrade", "Downgrade"],
-        default: 'Pending'
-    },
-    totalExpenditure: {
-        type: Number
-    }
-})
+  pointsAvailable: {
+    type: Number,
+    default: 0,
+  },
+  pointsForRedemptions: {
+    type: Number,
+    default: 0,
+  },
+  lastVisit: {
+    type: Date,
+  },
+  lastCommunication: {
+    type: Date,
+  },
+  lastMarketingCommunication: {
+    type: Date,
+  },
+  expiringPoints: {
+    type: Number,
+  },
+  lastUsagePoints: {
+    type: Number,
+  },
+  totalLifetimePoints: {
+    type: Number,
+  },
+  status: {
+    type: String,
+    enum: ["Active", "Pending", "Inactive", "Upgrade", "Downgrade"],
+    default: "Pending",
+  },
+  totalExpenditure: {
+    type: Number,
+  },
+});
 
-const TransactionalSchema = new mongoose.Schema({
+const TransactionalSchema = new mongoose.Schema(
+  {
     spendingType: {
-        type: String,
-        required: true,
-        enum: ["Life Café", "Yoga Class", "Vita Spa", "Retreats and YTT Packages", "Annual Membership Fee", "Bonus 1", "Bonus 2", "Bonus 3", "Bonus 4"]
+      type: String,
+      required: true,
+      enum: [
+        "Life Café",
+        "Yoga Class",
+        "Vita Spa",
+        "Retreats and YTT Packages",
+        "Annual Membership Fee",
+        "Bonus 1",
+        "Bonus 2",
+        "Bonus 3",
+        "Bonus 4",
+      ],
     },
     amount: {
-        type: Number,
-        required: true
+      type: Number,
+      required: true,
     },
     pointsGained: {
-        type: Number
+      type: Number,
     },
     tranCode: {
-        type: String
-    }
-    }, {
-    timestamps: true
-})
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const RedeemSchema = new mongoose.Schema({
+const RedeemSchema = new mongoose.Schema(
+  {
     reward: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     pointsLost: {
-        type: Number
+      type: Number,
     },
     tranCode: {
-        type: String
-    }
-    }, {
-    timestamps: true
-})
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
+      type: String,
     },
     email: {
-        type: String,
-        unique: true
+      type: String,
+      unique: true,
     },
     mobile: {
-        type: String,
-        unique: true
+      type: String,
+      unique: true,
+      sparse: true,
     },
     password: {
-        type: String,
+      type: String,
     },
     language: {
-        type: String,
-        required: true,
-        enum: ["English", "Thai"],
-        default: "English"
+      type: String,
+      required: true,
+      enum: ["English", "Thai"],
+      default: "English",
     },
     dob: {
-        type: Date,
+      type: Date,
     },
     isVerified: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false,
     },
     isAdmin: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false,
     },
     isSuperAdmin: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false,
     },
     isMember: {
-        type: Boolean,
-        default: true
+      type: Boolean,
+      default: true,
     },
     isVendor: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false,
     },
     memberId: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     googleId: String,
     facebookId: String,
@@ -168,30 +195,34 @@ const userSchema = new mongoose.Schema({
     transaction: [TransactionalSchema],
     redeem: [RedeemSchema],
     tier: {
-        type: String,
-        required: true,
-        default: "Balance",
-        enum: ["Balance", "Vitality", "Harmony", "Serenity"]
+      type: String,
+      required: true,
+      default: "Balance",
+      enum: ["Balance", "Vitality", "Harmony", "Serenity"],
     },
     referredBy: {
-        type: String
+      type: String,
     },
     referredTo: {
-        type: [String], 
-        default: []
-    }
-},
-    {
-        timestamps: true
-    })
+      type: [String],
+      default: [],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-userSchema.pre('save', function(next) {
-    if (this.email && (this.email.endsWith('@vikasa.com') ||  this.email.endsWith('@dosink.com'))) {
-        this.isAdmin = true;
-    }
-    next()
-})
+userSchema.pre("save", function (next) {
+  if (
+    this.email &&
+    (this.email.endsWith("@vikasa.com") || this.email.endsWith("@dosink.com"))
+  ) {
+    this.isAdmin = true;
+  }
+  next();
+});
 
-const User = mongoose.model("User", userSchema)
+const User = mongoose.model("User", userSchema);
 
-module.exports = User
+module.exports = User;
